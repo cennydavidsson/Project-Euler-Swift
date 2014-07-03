be4366ecbd55386b4aebbb2fa5eb93a9c84f0f82
Cenny Davidsson <cenny@Cennys-iMac.local>
2014-07-03 19:30:05 +0200

    Initial Commit

---
+++ Project euler/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Project euler
+//
+//  Created by Cenny Davidsson on 2014-07-03.
+//  Copyright (c) 2014 Cenny. All rights reserved.
+//
+
+import Foundation
+
+println("Hello, World!")
+
